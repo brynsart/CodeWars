b6371909d638bc33d8088788529c1b26c9935d2f
brynsart <brynsartain07@gmail.com>
2025-03-20 13:42:02 +0000

    Add files via upload

---
+++ codewars2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public static class Kata
+{
+    public static int Solution(int value)
+    {
+        List<int> div3 = new List<int>();
+        List<int> div5 = new List<int>();
+        int i = 1;
+        int sum = 0;
+
+        if (value > 0)
+        {
+            while (true)
+            {
+                if (3 * i >= value)
+                {
+                    break;
+                }
+                div3.Add(3 * i);
+                i++;
+            }
+            i = 0;
+            while (true)
+            {
+                if (5 * i >= value)
+                {
+                    break;
+                }
+                if (!div3.Contains(5 * i))
+                {
+                    div5.Add(5 * i); // So div by 3 and 5 not counted twice
+                }
+                i++;
+            }
+            sum = div3.Sum() + div5.Sum();
+            return sum;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
---
+++ codewars3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class NewAverage
+{
+    public static long NewAvg(double[] arr, double navg)
+    {
+        double total = 0;
+        double intermediate;
+        long donation;
+        // your code
+        for (int i = 0; i < arr.Length; i++)
+        {
+            total += arr[i];
+        }
+        intermediate = navg * (arr.Length + 1);
+        donation = (long)Math.Ceiling(intermediate - total);
+        if (donation <= 0)
+        {
+            throw new ArgumentException("donation less than or equal to 0");
+        }
+        return donation;
+    }
+}
